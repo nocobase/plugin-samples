e340ad5d393e63ec2c18c1541601dd6d24c18b3f
dream2023 <1098626505@qq.com>
2024-05-16 17:54:54 +0800

    fix: change dir name

--- packages/plugins/@nocobase-sample/plugin-provider-content/src/client/index.tsx
+++ packages/plugins/@nocobase-sample/plugin-provider-content/src/client/index.tsx
@@ -1,10 +1,10 @@
 import { Plugin } from '@nocobase/client';
 import { TopAnnouncement } from './TopAnnouncement';
 
-export class PluginProviderRenderClient extends Plugin {
+export class PluginProviderContentClient extends Plugin {
   async load() {
     this.app.addProvider(TopAnnouncement)
   }
 }
 
-export default PluginProviderRenderClient;
+export default PluginProviderContentClient;
