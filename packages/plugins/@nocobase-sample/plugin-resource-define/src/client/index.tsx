f3b6497c5393d3af1f3344254a49a9df9770f4c4
chenos <chenlinxh@gmail.com>
2024-09-18 16:18:16 +0800

    feat: update examples

--- packages/plugins/@nocobase-sample/plugin-resource-define/src/client/index.tsx
+++ packages/plugins/@nocobase-sample/plugin-resource-define/src/client/index.tsx
@@ -1,20 +1,64 @@
-import { Plugin } from '@nocobase/client';
+import { Plugin, useRequest } from '@nocobase/client';
+import { Spin } from 'antd';
+import React from 'react';
+
+// @ts-ignore
+import { name } from '../../package.json';
+
+function TestRequest1() {
+  const { data, loading } = useRequest({
+    url: 'testresourceManagerDefine:send1',
+  });
+  if (loading) {
+    return <Spin />
+  }
+  return <div>Response1: {data?.['data']}</div>
+}
+
+function TestRequest2() {
+  const { data, loading } = useRequest({
+    url: 'testresourceManagerDefine:send2',
+  });
+  if (loading) {
+    return <Spin />
+  }
+  return <div>Response2: {data?.['data']}</div>
+}
+
+function TestRequest3() {
+  const { data, loading } = useRequest({
+    url: 'testresourceManagerDefine:send3',
+  });
+  if (loading) {
+    return <Spin />
+  }
+  return <div>Response3: {data?.['data']}</div>
+}
+
+function MySettingPage() {
+  return (
+    <div>
+      <TestRequest1 />
+      <TestRequest2 />
+      <TestRequest3 />
+    </div>
+  )
+}
 
 export class PluginResourceDefineClient extends Plugin {
   async afterAdd() {
     // await this.app.pm.add()
   }
 
-  async beforeLoad() {}
+  async beforeLoad() { }
 
   // You can get and modify the app instance here
   async load() {
-    console.log(this.app);
-    // this.app.addComponents({})
-    // this.app.addScopes({})
-    // this.app.addProvider()
-    // this.app.addProviders()
-    // this.app.router.add()
+    this.app.pluginSettingsManager.add(name, {
+      title: 'testresourceManagerDefine',
+      icon: 'ApiOutlined',
+      Component: MySettingPage,
+    });
   }
 }
 
--- packages/plugins/@nocobase-sample/plugin-resource-define/src/server/plugin.ts
+++ packages/plugins/@nocobase-sample/plugin-resource-define/src/server/plugin.ts
@@ -9,14 +9,32 @@ export class PluginResourceDefineServer extends Plugin {
     this.app.resourceManager.define({
       name: 'testresourceManagerDefine',
       actions: {
-        send: async (ctx, next) => {
-          ctx.body = 'ok';
+        send1: async (ctx, next) => {
+          ctx.body = 'ok1';
+          await next();
+        },
+        send2: async (ctx, next) => {
+          ctx.body = 'ok2';
+          await next();
+        },
+        send3: async (ctx, next) => {
+          ctx.body = 'ok3';
           await next();
         },
       },
     });
-    // 公开访问，仅用于测试
-    this.app.acl.allow('testresourceManagerDefine', 'send', 'public');
+    // 公开访问
+    this.app.acl.allow('testresourceManagerDefine', 'send1', 'public');
+    // 仅登录用户可以访问
+    this.app.acl.allow('testresourceManagerDefine', 'send2', 'loggedIn');
+    // 由角色配置，需要后台操作
+    // URL: http://localhost:13000/admin/settings/users-permissions/roles
+    this.app.acl.registerSnippet({
+      name: `pm.${this.name}`,
+      actions: [
+        'testresourceManagerDefine:send3',
+      ],
+    });
   }
 
   async install() {}
